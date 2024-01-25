b192637bc794abaf5d0cf5eda14c4b7e97bbaa2e
Yiğit Bekiroğlu <124576682+Yigitbekiroglu07@users.noreply.github.com>
2024-01-25 18:14:31 +0300

    C# Temelleri 2
    C# Kampı 2.Gün

---
+++ 2.Gün/Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Metotlar;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+       /* string productname = "Elma";
+        double fiyat = 15;
+        string acıklama = "Amasya elması"; */
+
+        Urun urun1 = new Urun(); // dizi yerine class oluşturduğumuz zaman o ürünün tüm özelliklerini
+        // tek çatı altında toplamış oluruz.
+        // OLuşturulan classı kullanabilmek için onu newlemek gerekir.
+        urun1.Id = 1;
+        urun1.Adi = "Elma";
+        urun1.Fiyati = 15;
+        urun1.Açıklama = "Amasya elması";
+
+        Urun urun2 = new Urun();
+        urun2.Id = 2;
+        urun2.Adi = "Karpuz";
+        urun2.Fiyati = 80;
+        urun2.Açıklama = "Diyarbakır karpuzu";
+
+        Urun[] urunler = new Urun[] { urun1, urun2 }; // String gibi dizi oluşturabiliriz.
+        foreach (Urun urun in urunler)
+        {
+            Console.WriteLine(urun.Id);
+            Console.WriteLine(urun.Adi);
+            Console.WriteLine(urun.Açıklama);
+            Console.WriteLine(urun.Fiyati);
+            Console.WriteLine("------------");
+        }
+        Console.WriteLine("---------Metodlar---------");
+        SepetManager sepetManager = new SepetManager(); //Class'ı çağırmak için onu newlemek gerek.
+        sepetManager.Ekle(urun1);
+        sepetManager.Ekle(urun2);
+        sepetManager.Ekle2("Armut", "Yeşil Armut", 10);
+    }
+
+}
---
+++ 2.Gün/Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        public void Ekle(Urun aa) //Urun tiptir, urun ise aşağıda yazarken kullanılacak isimdir.
+        {
+            Console.WriteLine("Sepete eklendi : " +aa.Adi);
+        }
+        public void Ekle2(string urunAdi, string açıklama, double fiyat )
+        // Üstteki gibi yerine, class oluşturmadan bu şeiklde de yapılabilir.
+        // ancak bu kullanım pek yararlı değil, 
+        // sebebi yeni bir parametre girilmesi istendiğinde Program.cs'de her bir fonksiyonu değişitmemiz gerekecek.
+        {
+            Console.WriteLine("Sepete eklendi : " +urunAdi);
+        }
+    }
+}
